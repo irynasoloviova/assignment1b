540f8fe586537580cff1c124cfa1e5021840ee95
irynasoloviova <soloviova.i@northeastern.edu>
2020-09-15 21:42:33 -0400

    added comments to code

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,21 @@
+// define myImage as image on the website
 let myImage = document.querySelector('img');
 
+//change an image on a click
 myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute('src','images/firefox2.png');
-    } else {
-      myImage.setAttribute('src','images/firefox-icon.png');
-    }
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/firefox-icon.png') {
+    myImage.setAttribute('src','images/firefox2.png');
+  } else {
+    myImage.setAttribute('src','images/firefox-icon.png');
+  }
 }
 
+//buttons definition
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
+//changes the header if the username is entered
 function setUserName() {
   let myName = prompt('Please enter your name.');
   if(!myName) {
@@ -22,7 +26,7 @@ function setUserName() {
   }
 }
 
+//sets function above to a button
 myButton.onclick = function() {
   setUserName();
 }
-// Put your JS here.
